ce7c9465c5c4e3341a2ed5b34b9ca19e9a6724d3
diskominfolutra <diskominfolutra@gmail.com>
2025-07-23 23:55:56 +0700

    Menambahkan variabel fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
         'password',
+        'role',
+        'id_stakeholder',
+        'id_lembaga'
     ];
 
     /**
